95efc4ea214f2beea25849f2f07d3d458b2dc946
Abhishek Tyagi <atyagi2@node1x12c.cs.rochester.edu>
2025-03-03 13:09:47 -0500

    minor fix

--- timm/utils/compK.py
+++ timm/utils/compK.py
@@ -2,18 +2,18 @@ import torch
 import numpy as np
 
 # Let's call this function to compute k for each matrix
-    def compute_k_for_param(mask_shape,sparsity):
-        # shape is (out_features, in_features)
-        # You might do something like:
-        M, N = shape
+def compute_k_for_param(mask_shape,sparsity):
+    # shape is (out_features, in_features)
+    # You might do something like:
+    M, N = shape
 
-        num_rows, num_cols = mask_shape
-        # Diagonal length is the smaller dimension:
-        diagLen = num_cols if num_rows >= num_cols else num_rows
+    num_rows, num_cols = mask_shape
+    # Diagonal length is the smaller dimension:
+    diagLen = num_cols if num_rows >= num_cols else num_rows
 
-        # Calculate total nonzero elements desired.
-        elemCount = int((1 - sparsity) * num_rows * num_cols)
-        # Number of full diagonals we need to cover elemCount:
-        numDiag = elemCount // diagLen + 1
+    # Calculate total nonzero elements desired.
+    elemCount = int((1 - sparsity) * num_rows * num_cols)
+    # Number of full diagonals we need to cover elemCount:
+    numDiag = elemCount // diagLen + 1
 
-        return numDiag
+    return numDiag
